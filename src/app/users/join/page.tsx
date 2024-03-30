6e42a085507c6b724c047f30a3bcaa3887ff427e
gkstnadl <gkstnadl12@naver.com>
2024-03-31 03:39:11 +0900

    Feat : 로그인/회원가입 페이지 뼈대 만들기

--- src/app/users/join/page.tsx
+++ src/app/users/join/page.tsx
@@ -1,5 +1,38 @@
+const JOIN_FORM = [
+  {
+    type: 'text',
+    id: 'userId',
+    placeholder: 'ID',
+    error: 'E-Mail 형식으로 작성해 주세요.'
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'PassWord',
+    error: '숫자, 문자, 특수문자 조합으로 8자이상 작성해 주세요.'
+  },
+  {
+    type: 'text',
+    id: 'nickname',
+    placeholder: 'Nickname',
+    error: '2 ~ 15자로 작성해 주세요.'
+  }
+];
+
 const JoinPage = () => {
-  return <div>JoinPage</div>;
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <form>
+        {JOIN_FORM.map(({ type, id, placeholder, error }) => (
+          <label key={id}>
+            <input type={type} id={id} placeholder={placeholder} />
+          </label>
+        ))}
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 };
 
 export default JoinPage;
