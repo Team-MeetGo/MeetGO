491878ca2e91c6e13580058cc4c1a0e021362ad9
gkstnadl <gkstnadl12@naver.com>
2024-04-01 07:52:41 +0900

    Feat : supabase 및 테이블 타입 추출하여 세팅

---
+++ src/components/user/JoinForm.tsx
@@ -0,0 +1,20 @@
+const JOIN_FORM = [
+  {
+    type: 'text',
+    id: 'userId',
+    placeholder: 'ID',
+    error: 'E-Mail 형식으로 작성해 주세요.'
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'PassWord',
+    error: '숫자, 문자, 특수문자 조합으로 8자이상 작성해 주세요.'
+  },
+  {
+    type: 'text',
+    id: 'nickname',
+    placeholder: 'Nickname',
+    error: '2 ~ 15자로 작성해 주세요.'
+  }
+];
