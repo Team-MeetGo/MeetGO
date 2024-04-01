6a522621a447b32deb7f5eadcd5afd7e420f5a16
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-01 14:12:31 +0900

    [feat/#2] meetingroom 리스트 불러오기

--- src/app/meetingRoom/page.tsx
+++ src/app/meetingRoom/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { supabase } from '(@/utils/supabase)';
 
-const LobbyPage = () => {
-  return <div>LobbyPage</div>;
+const LobbyPage = async () => {
+  const { data: meetingroom, error } = await supabase.from('room').select(`*`).eq('going_chat', false);
+  if (error) return alert('error 발생!');
+
+  return (
+    <>
+      <article>
+        {/* <MeetingRoomForm /> */}
+        {meetingroom?.map((room) => (
+          <main key={room.room_id} className="w-full h-32 border-red-600 border-8">
+            <div> {room.room_title} </div>
+            <div> {room.feature} </div>
+            <div> {room.location} </div>
+            <div> {room.status} </div>
+            <div> {room.member_number}</div>
+            {/* <DeleteMeetingRoom id={room.room_id} />
+            <EditMeetingRoom /> */}
+          </main>
+        ))}
+      </article>
+    </>
+  );
 };
 
 export default LobbyPage;
