1072d287be9d5c441e5f51ce7706b11ae233588a
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-01 20:40:53 +0900

    [feat/#17] - meetingRoom 삭제하기

--- src/components/room/MeetingRoom.tsx
+++ src/components/room/MeetingRoom.tsx
@@ -1,17 +1,19 @@
 import type { Database } from '(@/types/database.types)';
+import DeleteMeetingRoom from './DeleteMeetingRoom';
+import EditMeetingRoom from './EditMeetingRoom';
 
 type MeetingRoom = Database['public']['Tables']['room']['Row'];
 
 function MeetingRoom({ list }: { list: MeetingRoom[] }) {
   return list.map((room) => (
-    <main key={room.room_id} className="w-full h-32 border-red-600 border-8">
+    <main key={room.room_id} className="w-full h-40 border-red-600 border-8">
       <div> {room.room_title} </div>
       <div> {room.feature} </div>
       <div> {room.location} </div>
       <div> {room.room_status} </div>
       <div> {room.member_number}</div>
-      {/* <DeleteMeetingRoom id={room.room_id} />
-    <EditMeetingRoom /> */}
+      <DeleteMeetingRoom id={room.room_id} />
+      <EditMeetingRoom room={room} />
     </main>
   ));
 }
