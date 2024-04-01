a69daefc563ecd384974c768e98c1e801a186261
injeong Hwang <twilight9758@gmail.com>
2024-04-01 11:50:10 +0900

    [feat/#4]-초기세팅: @supabase/ssr 패키지 설치 및 supabase 초기설정

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,7 @@
 import { Database } from '(@/types/database.types)';
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
-export const supabase = createClient<Database>(
+export const clientSupabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
