66918a32ddd05bbfb7a5d026271a38e55fb70833
injeong Hwang <twilight9758@gmail.com>
2024-04-02 20:41:22 +0900

    [feat/#33] - 불필요한 컴포넌트 depth 삭제(ChatParentList.tsx)

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react';
 import ChatInput from '../../components/chat/ChatInput';
-import ChatListParent from '(@/components/chat/ChatListParent)';
 import { serverSupabase } from '(@/utils/supabase/server)';
 import ChatHeader from '(@/components/chat/ChatHeader)';
+import ChatList from '(@/components/chat/ChatList)';
 
 const ChatPage = async () => {
   const supabase = serverSupabase();
@@ -16,15 +16,14 @@ const ChatPage = async () => {
     .eq('user_id', String(user?.id));
   console.log(userData);
 
-  // const {data: leader_id} = await supabase.from("")
+  const { data: serverMsg } = await supabase.from('messages').select('*');
 
   return (
     <div className="max-w-3xl mx-auto md:py-10 h-screen">
       <div className="h-full border rounded-md flex flex-col border-indigo-600">
         <ChatHeader />
-        {/* 메세지영역 */}
         <Suspense fallback="응애 나 애기 폴백">
-          <ChatListParent user={user} />
+          <ChatList serverMsg={serverMsg ?? []} user={user} />
         </Suspense>
         <ChatInput userData={userData} />
       </div>
