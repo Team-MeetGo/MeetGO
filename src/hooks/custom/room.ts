f786569de0bf1ebf1a466659a8e29f04b2886b1a
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-02 20:31:47 +0900

    [feat/#18] - meetingRoom에 새로운 방 추가시 가장 위로 올라오게 설정

--- src/hooks/custom/room.ts
+++ src/hooks/custom/room.ts
@@ -2,13 +2,21 @@ import { clientSupabase } from '(@/utils/supabase/client)';
 
 function meetingRoomHandler() {
   const getMeetingRoom = async () => {
-    const { data: meetingroom, error } = await clientSupabase.from('room').select(`*`).eq('going_chat', false);
+    const { data: meetingroom, error } = await clientSupabase
+      .from('room')
+      .select(`*`)
+      .eq('going_chat', false)
+      .order('created_at', { ascending: false });
     if (error) return alert('error 발생!');
     return meetingroom;
   };
 
   const getChattingRoom = async () => {
-    const { data: chattingRoom, error } = await clientSupabase.from('room').select(`*`).eq('going_chat', true);
+    const { data: chattingRoom, error } = await clientSupabase
+      .from('room')
+      .select(`*`)
+      .eq('going_chat', true)
+      .order('created_at', { ascending: false });
     if (error) return alert('error 발생!');
     return chattingRoom;
   };
