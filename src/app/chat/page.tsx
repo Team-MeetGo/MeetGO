29bacd8f8302abc4298b10894cabc9d92666b49c
seoyeon-1206 <ssy_1206@naver.com>
2024-04-03 14:09:35 +0900

    [feat/#1] - 지도 띄우기

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,6 +3,7 @@ import ChatInput from '../../components/chat/ChatInput';
 import ChatListParent from '(@/components/chat/ChatListParent)';
 import { serverSupabase } from '(@/utils/supabase/server)';
 import ChatHeader from '(@/components/chat/ChatHeader)';
+import Map from '(@/components/chat/map/Map)';
 
 const ChatPage = async () => {
   const supabase = serverSupabase();
@@ -19,16 +20,19 @@ const ChatPage = async () => {
   // const {data: leader_id} = await supabase.from("")
 
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className="h-full border rounded-md flex flex-col border-indigo-600">
-        <ChatHeader />
-        {/* 메세지영역 */}
-        <Suspense fallback="응애 나 애기 폴백">
-          <ChatListParent user={user} />
-        </Suspense>
-        <ChatInput userData={userData} />
+    <>
+      <Map />
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className="h-full border rounded-md flex flex-col border-indigo-600">
+          <ChatHeader />
+          {/* 메세지영역 */}
+          <Suspense fallback="응애 나 애기 폴백">
+            <ChatListParent user={user} />
+          </Suspense>
+          <ChatInput userData={userData} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
