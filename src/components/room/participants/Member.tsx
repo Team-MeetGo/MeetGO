ceb4c70a96b587384cc6c0ebef4c59c6aa38edac
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-04 00:40:11 +0900

    [feat/#19] - 수락창 생성하기

--- src/components/room/participants/Member.tsx
+++ src/components/room/participants/Member.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { clientSupabase } from '(@/utils/supabase/client)';
 import { useEffect, useState } from 'react';
-import RealTimeSubscription from './RealTimeSubscription';
 import { Database } from '(@/types/database.types)';
-import { UUID } from 'crypto';
 
+import type { UUID } from 'crypto';
 type ParticipantType = Database['public']['Tables']['participants']['Row'];
 
 const Member = ({ params }: { params: { id: UUID } }) => {
@@ -15,13 +14,42 @@ const Member = ({ params }: { params: { id: UUID } }) => {
 
       if (!participantList || participantList === null) return;
       setParticipants(participantList);
+
+      const channels = clientSupabase
+        .channel('custom-insert-channel')
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'participants'
+          },
+          (payload) => {
+            setParticipants((addMember) => [...addMember, payload.new as ParticipantType]);
+          }
+        )
+        .subscribe();
+
+      const deletechannels = clientSupabase
+        .channel('custom-delete-channel')
+        .on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'participants' }, (payload) => {
+          const filterdMember = participants.filter((member) => member.part_id !== payload.old.part_id);
+          setParticipants(() => filterdMember);
+        })
+        .subscribe();
+
+      return () => {
+        clientSupabase.removeChannel(channels);
+      };
     };
     participantsList();
   }, [params]);
-  console.log('memberparticipants', participants);
   return (
     <>
-      <RealTimeSubscription participantInsert={participants} />
+      {participants.map((member) => (
+        <div key={member.part_id}>{member.user_id} </div>
+      ))}
+      {/* <RealTimeSubscription participantInsert={participants} /> */}
     </>
   );
 };
