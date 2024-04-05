eb6d10a72976797d9068399bbda037e46dd771b6
seoyeon-1206 <ssy_1206@naver.com>
2024-04-05 23:28:32 +0900

    [feat/#59] 시간 선택시 텍스트로 표시

--- src/app/chat/[chatroom_id]/page.tsx
+++ src/app/chat/[chatroom_id]/page.tsx
@@ -34,7 +34,7 @@ const ChatPage = async ({ params }: { params: { chatroom_id: string } }) => {
 
   return (
     <div className="flex felx-row ">
-      <SideBar userId={user?.id} leaderId={leaderId} />
+      <SideBar userId={user?.id} leaderId={leaderId} chatRoomId={chatRoomId} />
       <div className="max-w-3xl mx-auto md:py-10 h-screen">
         <div className="h-full border rounded-md flex flex-col border-indigo-600 relative">
           <InitChat chatRoomId={chatRoomId} allMsgs={allMsgs ?? []} />
--- src/components/chat/sidebar/SideBar.tsx
+++ src/components/chat/sidebar/SideBar.tsx
@@ -3,19 +3,35 @@
 import React, { useState } from 'react';
 import Map from '(@/components/chat/sidebar/Map)';
 import DatePicker from './DatePicker';
-import { User } from '@supabase/supabase-js';
+import { clientSupabase } from '(@/utils/supabase/client)';
 
 interface SideBarProps {
   userId: string | null | undefined;
   leaderId: string | null | undefined;
+  chatRoomId: string | null | undefined;
 }
 
-const SideBar: React.FC<SideBarProps> = ({ userId, leaderId }) => {
+const SideBar: React.FC<SideBarProps> = ({ userId, leaderId, chatRoomId }) => {
   const [selectedLocation, setSelectedLocation] = useState<string>('');
+  const [selectedDateTime, setSelectedDateTime] = useState<string>('');
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+  const [finalDateTime, setFinalDateTime] = useState<string>('');
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleSelectedTime = async () => {
+    setFinalDateTime(selectedDateTime);
+    if (chatRoomId) {
+      const { data: insertedTime, error: insertError } = await clientSupabase
+        .from('chatting_room')
+        .update({ meeting_time: selectedDateTime })
+        .eq('chatting_room_id', chatRoomId);
+      console.log('시간', selectedDateTime);
+    }
+  };
+    console.log('시간', selectedDateTime);
   };
 
   return (
@@ -23,10 +39,19 @@ const SideBar: React.FC<SideBarProps> = ({ userId, leaderId }) => {
       <button onClick={toggleSidebar}>사이드바</button>
       {isSidebarOpen && (
         <div>
-          미팅 날짜/시간: <input type="text" className="border" />
-          <button>날짜 선택</button>
+          <div>
+            미팅 날짜/시간:
+            <input
+              type="text"
+              className="border"
+              value={selectedDateTime}
+              onChange={(e) => setSelectedDateTime(e.target.value)}
+            />
+            <button onClick={handleSelectedTime}>날짜 선택</button>
+            <p>선택한 날짜 시간이 보여진다. {finalDateTime}</p>
+          </div>
           <DatePicker />
-          <div>미팅 장소 : {selectedLocation}</div>
+          <p>미팅 장소 : {selectedLocation}</p>
           <Map setSelectedLocation={setSelectedLocation} userId={userId} leaderId={leaderId} />
         </div>
       )}
