34be741b86b7d703d8df1c2004500395ab230e1b
seoyeon-1206 <ssy_1206@naver.com>
2024-04-05 19:24:21 +0900

    [merge/#59] pull 받기 전 커밋

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,6 @@
-import { Suspense } from 'react';
+'use client';
+
+import { Suspense, useState } from 'react';
 import { serverSupabase } from '(@/utils/supabase/server)';
 import ChatHeader from '(@/components/chat/chatHeader/ChatHeader)';
 import ChatList from '(@/components/chat/chatBody/ChatList)';
@@ -9,15 +11,21 @@ const ChatPage = async () => {
   const supabase = serverSupabase();
   const { data } = await supabase.auth.getUser();
   const user = data.user;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   const { data: allMsgs } = await supabase.from('messages').select('*').order('created_at', { ascending: false });
 
   return (
     <div className="flex felx-row ">
-      <div className="border">
-        <SideBar />
+      <div className={`border ${isSidebarOpen ? 'w-72' : 'w-0'} overflow-hidden transition-all duration-300`}>
+        {isSidebarOpen && <SideBar />}
       </div>
       <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <button onClick={toggleSidebar}> 사이드 바 </button>
         <div className="h-full border rounded-md flex flex-col border-indigo-600 relative">
           <ChatHeader allMsgs={allMsgs ?? []} />
           <Suspense fallback="응애 나 애기 폴백">
