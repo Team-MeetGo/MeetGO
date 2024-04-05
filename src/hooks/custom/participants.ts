c1dd53df52eb0b8414d58b789df22ca5d5a4c131
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-05 09:35:27 +0900

    [del/#49] - 기능없는 코드 삭제

--- src/hooks/custom/participants.ts
+++ src/hooks/custom/participants.ts
@@ -5,25 +5,6 @@ import type { UUID } from 'crypto';
 type UserType = Database['public']['Tables']['users']['Row'];
 
 function participants() {
-  const getTotalMember = async ({ roomId }: { roomId: UUID }) => {
-    const { data: getTotalMemberList, error } = await clientSupabase
-      .from('participants')
-      .select(`*`)
-      .eq('room_id', roomId)
-      .select(`user_id, users(*)`);
-
-    if (error) return alert('구성원 정보 오류 발생!');
-    const totalMemberList: UserType[] = getTotalMemberList.map((member) => {
-      return member.users as UserType;
-    });
-    if (!totalMemberList || totalMemberList.length < 1) return;
-
-    const getFemaleMember = totalMemberList.filter((member) => member.gender === 'female');
-    const getMaleMember = totalMemberList.filter((member) => member.gender === 'male');
-
-    return { getFemaleMember, getMaleMember };
-  };
-
   const totalMember = async (room_id: string) => {
     let { data: totalParticipants, error } = await clientSupabase
       .from('participants')
@@ -40,19 +21,16 @@ function participants() {
     const { data, error } = await clientSupabase.from('participants').insert([{ user_id, room_id }]);
   };
 
-  const addMemeberHandler = (room_id: string) => {
-    const addNewMember = async () => {
-      const { data } = await clientSupabase.auth.getUser();
-      if (!data.user) {
-        return alert('잘못된 접근입니다.');
-      }
-      const user_id = data.user.id;
-      addMemeber({ user_id, room_id });
-    };
-    return addNewMember();
+  const addMemeberHandler = async (room_id: string) => {
+    const { data } = await clientSupabase.auth.getUser();
+    if (!data.user) {
+      return alert('잘못된 접근입니다.');
+    }
+    const user_id = data.user.id;
+    addMemeber({ user_id, room_id });
   };
 
-  return { getTotalMember, deleteMember, addMemeber, addMemeberHandler, totalMember };
+  return { deleteMember, addMemeber, addMemeberHandler, totalMember };
 }
 
 export default participants;
