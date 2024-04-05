5e7146dc75e2ba3e6c9c6988333d6c25dcca7996
kewii33 <kewiii071@gmail.com>
2024-04-05 20:56:18 +0900

    [feat/#77] - 메인페이지에 리뷰 좋아요 순으로 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,57 @@
 'use client';
 
+import ReviewCard from '(@/components/review/ReviewCard)';
+import { reviewData } from '(@/components/review/ReviewList)';
 import { signOut } from '(@/utils/api/authAPI)';
+import { clientSupabase } from '(@/utils/supabase/client)';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [reviewData, setReviewData] = useState<reviewData[]>([]);
+
+  async function getMostLikedReview() {
+    const likedReviewIds = (await clientSupabase.from('review_like').select('review_id')).data?.map(
+      (item) => item.review_id
+    );
+
+    const { data: allReviews } = await clientSupabase.from('review').select('*');
+
+    const zeroLikedReviews = allReviews?.filter((review) => !likedReviewIds?.includes(review.review_id));
+
+    const likedReviews = allReviews?.filter((review) => likedReviewIds?.includes(review.review_id));
+
+    likedReviews?.sort((a, b) => {
+      const aLikes = likedReviewIds?.filter((id) => id === a.review_id).length || 0;
+      const bLikes = likedReviewIds?.filter((id) => id === b.review_id).length || 0;
+      return bLikes - aLikes;
+    });
+
+    const sliceReviews = [...(likedReviews || []), ...(zeroLikedReviews || [])].slice(0, 6);
+
+    setReviewData(sliceReviews);
+  }
+
+  useEffect(() => {
+    getMostLikedReview();
+    console.log(getMostLikedReview);
+  });
+
   return (
     <main>
-      메인페이지 입니다.
+      <div className="">배너</div>
       <div>
-        <button onClick={signOut}>임시 로그아웃</button>
+        <button onClick={signOut}>임시 로그아웃 버튼입니다</button>
+      </div>
+      <div>
+        <div>
+          <Link href="/review">더보기</Link>
+        </div>
+        <ul className="grid grid-cols-3 gap-2 gap-y-4">
+          {reviewData.map((item, index) => (
+            <ReviewCard key={index} review={item} />
+          ))}
+        </ul>
       </div>
     </main>
   );
