b3e87dbd0d28f50b1c7e9204dc01bb354dbeac28
gkstnadl <gkstnadl12@naver.com>
2024-04-05 18:02:49 +0900

    Refactor : 중복되는 로직 통합 및 간소화, 수정버튼 누르면 전체 다 수정모드로 변경, 내가 쓴 글/좋아요글 링크 추가

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -6,6 +6,7 @@ const NavBar = async () => {
   const { data } = await supabase.auth.getUser();
   const user = data.user;
   const { data: userData } = await supabase.from('users').select('*').eq('user_id', String(user?.id));
+
   return (
     <>
       <InitUser userData={userData} />
