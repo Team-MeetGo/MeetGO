1e5525e822097df195e6e77dad10dddd167fafcd
injeong Hwang <twilight9758@gmail.com>
2024-04-08 21:53:37 +0900

    [feat/#109] - 채팅 페이지 로딩 중 spinner 적용

--- src/app/chat/[chatroom_id]/page.tsx
+++ src/app/chat/[chatroom_id]/page.tsx
@@ -5,6 +5,7 @@ import ChatList from '(@/components/chat/chatBody/ChatList)';
 import InitChat from '(@/components/chat/chatHeader/InitChat)';
 import SideBar from '(@/components/chat/sidebar/SideBar)';
 import ChatInput from '(@/components/chat/ChatInput)';
+import ChatLoading from '(@/components/chat/ChatLoading)';
 
 const ChatPage = async ({ params }: { params: { chatroom_id: string } }) => {
   const chatRoomId = params.chatroom_id;
@@ -33,19 +34,21 @@ const ChatPage = async ({ params }: { params: { chatroom_id: string } }) => {
     .order('created_at', { ascending: false });
 
   return (
-    <div className="flex felx-row">
-      <InitChat chatRoomId={chatRoomId} allMsgs={allMsgs ?? []} />
-      <SideBar userId={user?.id} leaderId={leaderId} chatRoomId={chatRoomId} />
-      <div className="w-full max-w-2xl mx-auto md:py-10 h-screen">
-        <div className="h-full border rounded-md flex flex-col border-indigo-600 relative">
-          <ChatHeader chatRoomId={chatRoomId} />
-          <Suspense fallback="skeleton 들어갈 자리">
-            <ChatList user={user} chatRoomId={chatRoomId} />
-          </Suspense>
-          <ChatInput />
+    <Suspense fallback={<ChatLoading />}>
+      <div className="flex felx-row">
+        <InitChat chatRoomId={chatRoomId} allMsgs={allMsgs ?? []} />
+        <SideBar userId={user?.id} leaderId={leaderId} chatRoomId={chatRoomId} />
+        <div className="w-full max-w-2xl mx-auto md:py-10 h-screen">
+          <div className="h-full border rounded-md flex flex-col border-indigo-600 relative">
+            <ChatHeader chatRoomId={chatRoomId} />
+            <Suspense fallback="skeleton 들어갈 자리">
+              <ChatList user={user} chatRoomId={chatRoomId} />
+            </Suspense>
+            <ChatInput />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
--- src/hooks/useQueries/useChattingQuery.ts
+++ src/hooks/useQueries/useChattingQuery.ts
@@ -1,14 +1,6 @@
 import { fetchRoomData, selectMessage } from '(@/services/queryFns)';
-import { CHATMESSAGE_QUERY_KEY, ROOMDATA_QUERY_KEY } from '(@/services/queryKeys)';
+import { ROOMDATA_QUERY_KEY } from '(@/services/queryKeys)';
 import { useSuspenseQuery } from '@tanstack/react-query';
-
-export const useChattingQuery = () => {
-  const { data, isLoading } = useSuspenseQuery({
-    queryKey: CHATMESSAGE_QUERY_KEY,
-    queryFn: selectMessage
-  });
-  return { data, isLoading };
-};
 
 export const useRoomDataQuery = (chatRoomId: string) => {
   const { data: room } = useSuspenseQuery({
