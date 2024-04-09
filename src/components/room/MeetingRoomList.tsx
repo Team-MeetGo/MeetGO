479198da145c24b59fb5e0a9c358efb6657d7e93
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-09 17:30:44 +0900

    [feat/#114] -내가 참여한 방 화살표로 좌우로 이동

--- src/components/room/MeetingRoomList.tsx
+++ src/components/room/MeetingRoomList.tsx
@@ -1,43 +1,34 @@
 'use client';
 import meetingRoomHandler from '(@/hooks/custom/room)';
-import { userStore } from '(@/store/userStore)';
+import { useRecruitingMyroomQuery } from '(@/hooks/useQueries/useMeetingQuery)';
 import { useEffect, useState } from 'react';
-import MeetingRoom from './MeetingRoom';
-
-import type { Database } from '(@/types/database.types)';
 import { IoMdRefresh } from 'react-icons/io';
+import MeetingRoom from './MeetingRoom';
 import MeetingRoomForm from './MeetingRoomForm';
-import { useRouter } from 'next/navigation';
 
-function MeetingRoomList() {
-  type MeetingRoom = Database['public']['Tables']['room']['Row'];
-  const router = useRouter();
-  const { user, isLoggedIn } = userStore((state) => state);
-  const [meetingRoomList, setMeetingRoomList] = useState<MeetingRoom[]>();
-  const [myRoomList, setMyRoomList] = useState<MeetingRoom[]>();
-  const [chattingRoomList, setChattingRoomList] = useState<MeetingRoom[]>();
+import type { MeetingRoomType, MeetingRoomTypes } from '(@/types/roomTypes)';
+import type { User } from '@supabase/supabase-js';
+
+function MeetingRoomList({ user }: { user: User | null }) {
   const [page, setPage] = useState(1);
-  const { getMeetingRoom, getChattingRoom, getMyRoom } = meetingRoomHandler();
+  const result = useRecruitingMyroomQuery(user ? user?.id : null);
+  const recruitingRoom = result[0] as MeetingRoomTypes;
+  const myRoom = result[1]?.map((sample: any) => sample.room);
 
+  const [chattingRoomList, setChattingRoomList] = useState<MeetingRoomTypes>();
+  const { getChattingRoom } = meetingRoomHandler();
   useEffect(() => {
     const getMeetingRoomList = async () => {
-      const meetingroom = (await getMeetingRoom()) as MeetingRoom[];
-      const myRoomWithId = (await getMyRoom(user[0].user_id)) as any;
-      const myRoom = myRoomWithId.map((sample: any) => sample.room);
-      const chattingroom = (await getChattingRoom()) as MeetingRoom[];
-      setMeetingRoomList(meetingroom);
+      const chattingroom = (await getChattingRoom()) as MeetingRoomTypes;
       setChattingRoomList(chattingroom);
-      setMyRoomList(myRoom);
     };
     getMeetingRoomList();
-  }, [user]);
-
-  if (meetingRoomList === undefined) return;
+  }, []);
   if (chattingRoomList === undefined) return;
 
   // meetingRoomList 중에서 myRoomList가 없는 것을 뽑아내기
-  const otherRooms = meetingRoomList.filter(function (room: MeetingRoom) {
-    const foundItem = myRoomList?.find((r) => r.room_id === room.room_id);
+  const otherRooms = recruitingRoom?.filter(function (room: MeetingRoomType) {
+    const foundItem = myRoom?.find((r) => r.room_id === room?.room_id);
     if (foundItem) {
       return false;
     } else {
@@ -54,8 +45,8 @@ function MeetingRoomList() {
     }
     setPage((page) => page - 1);
   };
-  console.log(myRoomList);
-  console.log(page);
+  console.log('otherRooms', otherRooms);
+  console.log('myRoom', myRoom);
   return (
     <>
       <article>
@@ -73,11 +64,17 @@ function MeetingRoomList() {
             <button onClick={() => beforePage()}> - </button>
             {
               <div className="gap-8 grid grid-cols-3 m-4 w-full px-4">
-                {myRoomList?.map((room, index) => (
+                {myRoom?.map((room, index) => (
                   <div
                     key={index}
-                    className={`room ${page < 1 ? setPage(1) : page * 3 + 2 >= index && index >= page * 3 && 'hidden'}`}
+                    className={`room ${
+                      page < 1 || myRoom.length / 3 + 1 < page
+                        ? setPage(1)
+                        : 3 * page - 1 > index && 3 * page - 3 < index && 'hidden'
+                    }`}
                   >
+                    {page * 3 + 1} 시작
+                    {page * 3 - 1} 끝{index}
                     <MeetingRoom room={room} />
                   </div>
                 ))}
@@ -89,14 +86,14 @@ function MeetingRoomList() {
         <div className="m-4 text-xl	font-semibold">모집중</div>
         <div className="gap-2 grid grid-cols-3 m-4 w-100%">
           {otherRooms?.map((room) => (
-            <MeetingRoom key={room.room_id} room={room} />
+            <MeetingRoom key={room?.room_id} room={room} />
           ))}
         </div>
         {/* 이 부분은 채팅룸이 형성된 전체 방으로, 마지막에는 삭제 예정입니다. */}
         <div>========여기부터는 채팅방========</div>
         <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-8">
-          {chattingRoomList.map((room) => (
-            <MeetingRoom key={room.room_id} room={room} />
+          {chattingRoomList?.map((room) => (
+            <MeetingRoom key={room?.room_id} room={room} />
           ))}
         </div>
       </article>
