ccc60b1290747bb4621dec07c02f50fbf2e3cb0d
seoyeon-1206 <ssy_1206@naver.com>
2024-04-14 20:38:27 +0900

    [style/#139] react-icons 변경

--- src/components/chat/sidebar/DateCustomeInput.tsx
+++ src/components/chat/sidebar/DateCustomeInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MdOutlineDateRange } from 'react-icons/md';
-import { FaChevronDown } from 'react-icons/fa6';
+import { MdDateRange } from 'react-icons/md';
+import { IoIosArrowDown } from 'react-icons/io';
 
 interface DateCustomInputProps {
   onClick: () => void;
@@ -10,10 +10,10 @@ const DateCustomeInput: React.ForwardRefRenderFunction<HTMLDivElement, DateCusto
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
       <div className="border w-full h-[60px] flex items-center border-gray2 rounded-lg">
-        <MdOutlineDateRange size={25} color="#8F5DF4" className="ml-3.5" />
+        <MdDateRange size={25} color="#8F5DF4" className="ml-3.5" />
         <h1 className="flex-1 px-4 font-semibold text-2xl"> Date / Time Picker</h1>
         <button onClick={onClick}>
-          <FaChevronDown className="mr-3.5" />
+          <IoIosArrowDown size={24} className="mr-3.5" />
         </button>
       </div>
     </div>
