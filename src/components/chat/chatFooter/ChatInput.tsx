3fd0b3bc6c817ec75330eb2e455583fcf379689f
injeong Hwang <twilight9758@gmail.com>
2024-04-17 03:28:07 +0900

    [fix] - 불필요한 파일 삭제 / HowManyMsg.tsx

--- src/components/chat/chatFooter/ChatInput.tsx
+++ src/components/chat/chatFooter/ChatInput.tsx
@@ -3,17 +3,18 @@ import { useGetUserDataQuery } from '@/hooks/useQueries/useUserQuery';
 import { chatStore } from '@/store/chatStore';
 import { clientSupabase } from '@/utils/supabase/client';
 import { Input } from '@nextui-org/react';
-import { useState } from 'react';
-import { FaRegArrowAltCircleUp } from 'react-icons/fa';
+import { ChangeEvent, useState } from 'react';
+import { FaPlus, FaRegArrowAltCircleUp } from 'react-icons/fa';
 
 //이거 나중에 지우자
 const ChatInput = () => {
-  const [message, setMessage] = useState('');
   const { data: user } = useGetUserDataQuery();
   const chatRoomId = chatStore((state) => state.chatRoomId);
+  const [message, setMessage] = useState('');
+  const [imgs, setImgs] = useState<File[]>([]);
 
   const handleSubmit = async () => {
-    if (user && chatRoomId) {
+    if (user && chatRoomId && message.length) {
       const { error } = await clientSupabase.from('messages').insert({
         send_from: user.user_id,
         message: message,
@@ -26,26 +27,40 @@ const ChatInput = () => {
     }
   };
 
-  return (
-    <form
-      className="p-5 flex gap-2"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-        setMessage('');
-      }}
-    >
-      <Input
-        value={message}
-        classNames={{ input: ['bg-[#F2EAFA]', 'focus:outline-none'] }}
-        placeholder="send message"
-        onChange={(e) => setMessage(e.target.value)}
-      />
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const fileList = Array.from(files);
+      setImgs([...fileList]);
+    }
+  };
 
-      <button>
-        <FaRegArrowAltCircleUp className="h-8 w-8 my-auto" />
-      </button>
-    </form>
+  return (
+    <div>
+      <div className="flex items-center cursor-pointer">
+        <input type="file" multiple accept="image/*" className="" onChange={handleFile}></input>
+        <FaPlus className="w-6 h-6" />
+      </div>
+      <form
+        className="p-5 flex gap-[10px]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+          setMessage('');
+        }}
+      >
+        <Input
+          value={message}
+          classNames={{ input: ['bg-[#F2EAFA]', 'focus:outline-none'] }}
+          placeholder="send message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <button>
+          <FaRegArrowAltCircleUp className="h-8 w-8 my-auto" />
+        </button>
+      </form>
+    </div>
   );
 };
 
