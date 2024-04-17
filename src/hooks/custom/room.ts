60f53a1d32d528ace5d0e1f6873f6b4c4463cb29
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-17 19:26:02 +0900

    [del] - 기능하지 않는 코드 제거

--- src/hooks/custom/room.ts
+++ src/hooks/custom/room.ts
@@ -2,16 +2,6 @@
 import { clientSupabase } from '@/utils/supabase/client';
 
 function meetingRoomHandler() {
-  const getChattingRoom = async () => {
-    const { data: chattingRoom, error } = await clientSupabase
-      .from('room')
-      .select(`*`)
-      .eq('room_status', '모집종료')
-      .order('created_at', { ascending: false });
-    if (error) return alert('error 발생!');
-    return chattingRoom;
-  };
-
   const getmaxGenderMemberNumber = (memberNumber: string) => {
     if (memberNumber === '1:1') {
       return 1;
@@ -24,7 +14,7 @@ function meetingRoomHandler() {
     }
   };
 
-  return { getChattingRoom, getmaxGenderMemberNumber };
+  return { getmaxGenderMemberNumber };
 }
 
 export default meetingRoomHandler;
