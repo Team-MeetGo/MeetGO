d6e28fcd719e1a8257fbacd5a481caf82c981c1d
seoyeon-1206 <ssy_1206@naver.com>
2024-04-18 20:27:46 +0900

    [refactor/#194] sortedData 배열 key값을 comment_id로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,14 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="mt-[120px] mb-[88px]">
-        <div>
-          <GetMostLikedReivew />
-        </div>
+      <div className="my-12">
+        <GetMostLikedReivew />
       </div>
       <div className="w-full h-[208px] bg-gradient-to-main2 flex justify-center items-center mb-[88px]">
         <div className="flex items-center">
           <p className="text-[30px] text-white mr-[60px]">현재 많은 분들이 기다리고 있어요~</p>
           <Link
-            className="bg-transparant border-1 border-white max-w-[267px] h-[60px] text-[18px] text-white pt-[13px] pb-[13px] pl-[60px] pr-[60px] hover:bg-white hover:text-mainColor font-bold"
+            className="bg-transparant border-1 rounded-xl border-white max-w-[267px] h-[60px] text-[18px] text-white pt-[13px] pb-[13px] pl-[60px] pr-[60px] hover:bg-white hover:text-mainColor font-bold"
             href="/meetingRoom"
           >
             로비로 바로가기
