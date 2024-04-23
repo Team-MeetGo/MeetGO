8ea816ea036e43f7ef4133568362fe5d5321af19
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-04-23 21:49:43 +0900

    [feat] - 참여한 방이 없을 때 없다는 문구를 추가하고 간격을 조정

--- src/components/room/lobby/MyRoomTitle.tsx
+++ src/components/room/lobby/MyRoomTitle.tsx
@@ -3,19 +3,17 @@ import RefreshButoon from '@/components/room/lobby/RefreshButton';
 
 function MyRoomsTitle({ children }: { children: React.ReactNode }) {
   return (
-    <article className="bg-white w-full flex-col justify-center align-middle">
-      <div className="flex flex-row justify-center align-middle">
-        <section className="h-[366px] mt-[64px] border-b border-gray2 max-w-[1250px]">
-          <div className="flex flex-row w-full justify-between pb-[24px]">
-            <p className="text-[40px] font-semibold ml-[56px]">참여 중</p>
-            <div className="flex flex-row align-middle justify-center gap-4 mr-[56px]">
-              <RefreshButoon />
-              <MeetingRoomForm />
-            </div>
+    <article className="w-[1112px] flex-col justify-center align-middle">
+      <section className="mt-[64px] border-b border-gray2">
+        <div className="flex flex-row w-full justify-between pb-[24px]">
+          <p className="text-[40px] font-semibold ml-[56px]">참여 중</p>
+          <div className="flex flex-row align-middle justify-center gap-4 mr-[56px]">
+            <RefreshButoon />
+            <MeetingRoomForm />
           </div>
-          {children}
-        </section>
-      </div>
+        </div>
+        <section className="w-100% flex flex-row items-center justify-content mb-[40px]">{children}</section>
+      </section>
     </article>
   );
 }
