8488b6e9bbe9a6ea459a5e0e88a3faf1f55488bb
injeong Hwang <twilight9758@gmail.com>
2024-04-24 15:36:57 +0900

    [fix/#218] - 채팅방 나갈 시 문구 순서 수정

--- src/components/chat/chatHeader/ChatHeader.tsx
+++ src/components/chat/chatHeader/ChatHeader.tsx
@@ -101,8 +101,8 @@ const ChatHeader = ({ chatRoomId }: { chatRoomId: string }) => {
 
   // 필요한 것만 async/await 처리하기
   const getOutOfChatRoom = async () => {
-    const message = `한 번 나가면 다시 입장하실 수 없습니다. 
-    한명이라도 나가면 채팅방이 닫힙니다. 
+    const message = `한명이라도 나가면 채팅방이 종료됩니다. 
+    한 번 나가면 다시 입장하실 수 없습니다. 
     그래도 나가시겠습니까?`;
     openModal({
       type: 'confirm',
