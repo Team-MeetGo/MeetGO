d4c33aa2407cb25697107cb34bd44d93fe54c412
injeong Hwang <twilight9758@gmail.com>
2024-04-28 23:40:18 +0900

    [fix/#218] - old_navbar.tsx 삭제

--- src/components/mainpage/MainBottomBanner.tsx
+++ src/components/mainpage/MainBottomBanner.tsx
@@ -22,11 +22,11 @@ const MainBottomBanner = () => {
           </p>
         </div>
         <Link
-          className="bg-white rounded-xl max-w-52 px-7 h-12 text-secondMainColor text-base font-semibold my-auto flex justify-center items-center hover:bg-opacity-90"
+          className="bg-white rounded-xl max-w-52 px-7 h-12 text-secondMainColor text-base font-semibold my-auto flex justify-center items-center hover:bg-opacity-90 break-keep"
           href={isLoggedIn ? '/' : 'join'}
           onClick={checkIfMember}
         >
-          회원가입하러 가기
+          회원가입 하러가기
         </Link>
       </div>
     </section>
