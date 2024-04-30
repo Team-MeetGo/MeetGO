48d3f13eced266b49dd2d40b738ae5474f991101
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-05-01 06:26:24 +0900

    [fix] - 아바타 수정 등

--- src/components/room/lobby/MyRoomTitle.tsx
+++ src/components/room/lobby/MyRoomTitle.tsx
@@ -4,7 +4,7 @@ import RefreshButton from '@/components/room/lobby/RefreshButton';
 function MyRoomsTitle({ children }: { children: React.ReactNode }) {
   return (
     <article className="lg:w-[1000px] w-[22rem] flex flex-col items-center justify-content align-middle">
-      <section className="pt-[1rem] border-b border-gray2 ">
+      <section className="border-b border-gray2 ">
         <div className="flex flex-row items-center justify-content lg:w-full justify-between pb-[24px] w-[22rem]">
           <p className="lg:text-[25px] text-[1rem] font-semibold">참여 중</p>
           <div className="flex flex-row items-center justify-content gap-4">
