f510ba0728c88eda2e16bc612beda3feedbab49f
injeong Hwang <twilight9758@gmail.com>
2024-04-30 17:11:34 +0900

    [style/#218] - 채팅검색창 반응형 CSS

--- src/app/review/pageNumber/[pageNumber]/page.tsx
+++ src/app/review/pageNumber/[pageNumber]/page.tsx
@@ -1,14 +1,13 @@
 import ReviewList from '@/components/review/ReviewList';
 import ThinBanner from '@/utils/banner/ThinBanner';
-import Link from 'next/link';
 
 const ReviewsPage = () => {
   return (
     <>
       <main className="w-full max-w-[1080px] mx-auto px-[24px] flex flex-col gap-12">
         <header className="h-40 flex flex-col justify-center items-center gap-4 py-auto">
-          <p className="text-[#6F7785] mr-auto">자유게시판 이라고 생각해도 좋아요.</p>
           <h1 className="text-4xl font-extrabold mr-auto">실시간 후기</h1>
+          <p className="text-[#6F7785] mr-auto">자유게시판 이라고 생각해도 좋아요.</p>
         </header>
         <ReviewList />
       </main>
