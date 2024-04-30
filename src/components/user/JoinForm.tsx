fb5f8f8c3a4b324dab4247c601ae90c94e76474e
gkstnadl <gkstnadl12@naver.com>
2024-04-30 18:36:18 +0900

    style : 로그인, 회원가입 페이지 위아래 여백 늘림

--- src/components/user/JoinForm.tsx
+++ src/components/user/JoinForm.tsx
@@ -146,83 +146,90 @@ const JoinForm = () => {
 
   return (
     <>
-      <form className="max-w-[450px] flex flex-col gap-[16px] w-full px-6" onSubmit={onSubmitForm}>
-        <div>
-          <div className="flex gap-[16px] w-full">
-            <label key="nickname" className="w-full">
+      <div className="w-full m-auto flex flex-col justify-center items-center gap-4 h-full p-6 py-8">
+        <h1 className="text-4xl font-semibold">회원가입</h1>
+        <div className="flex flex-col items-center mb-[10px]">
+          <p>회원 정보를 입력해주세요.</p>
+          <p>비밀번호 찾기시 유효한 이메일이 필요합니다.</p>
+        </div>
+        <form className="max-w-[450px] flex flex-col gap-[16px] w-full" onSubmit={onSubmitForm}>
+          <div>
+            <div className="flex gap-[16px] w-full">
+              <label key="nickname" className="w-full">
+                <input
+                  className="p-5 border border-[#A1A1AA] placeholder:text-[#A1A1AA] placeholder:text-[14px] rounded-lg focus:outline-none focus:border-mainColor w-full"
+                  type="text"
+                  name="nickname"
+                  placeholder="닉네임을 입력해주세요."
+                  onChange={onChangeInput}
+                  minLength={2}
+                  maxLength={10}
+                  required
+                />
+              </label>
+              <div className="flex gap-[8px]">
+                <Button
+                  className={
+                    gender === 'female'
+                      ? 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg bg-mainColor text-white'
+                      : 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg border-[#A1A1AA]'
+                  }
+                  color={gender === 'female' ? 'secondary' : 'default'}
+                  variant={gender === 'female' ? 'solid' : 'bordered'}
+                  type="button"
+                  onClick={() => onGenderSelect('female')}
+                >
+                  여성
+                </Button>
+                <Button
+                  className={
+                    gender === 'male'
+                      ? 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg bg-mainColor text-white'
+                      : 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg border-[#A1A1AA]'
+                  }
+                  variant={gender === 'male' ? 'solid' : 'bordered'}
+                  type="button"
+                  onClick={() => onGenderSelect('male')}
+                >
+                  남성
+                </Button>
+              </div>
+            </div>
+            {!isValidateShow.nickname && (
+              <p className="text-red-500 text-[13px] mt-2">닉네임은 2-12글자 이내로 작성해주세요. </p>
+            )}
+          </div>
+          {JOIN_FORM_LIST.map(({ type, name, placeholder, error }) => (
+            <label key={name} className="relative">
               <input
                 className="p-5 border border-[#A1A1AA] placeholder:text-[#A1A1AA] placeholder:text-[14px] rounded-lg focus:outline-none focus:border-mainColor w-full"
-                type="text"
-                name="nickname"
-                placeholder="닉네임을 입력해주세요."
+                type={type}
+                name={name}
+                placeholder={placeholder}
                 onChange={onChangeInput}
-                minLength={2}
-                maxLength={10}
                 required
               />
+              {name === 'password' && (
+                <button type="button" onClick={togglePasswordShow} className="absolute top-6 right-4">
+                  {passwordShow ? <IoMdEye className="text-xl" /> : <IoMdEyeOff className="text-xl" />}
+                </button>
+              )}
+              {!isValidateShow[name] && <p className="text-red-500 text-[13px] mt-2">{error}</p>}
             </label>
-            <div className="flex gap-[8px]">
-              <Button
-                className={
-                  gender === 'female'
-                    ? 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg bg-mainColor text-white'
-                    : 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg border-[#A1A1AA]'
-                }
-                color={gender === 'female' ? 'secondary' : 'default'}
-                variant={gender === 'female' ? 'solid' : 'bordered'}
-                type="button"
-                onClick={() => onGenderSelect('female')}
-              >
-                여성
-              </Button>
-              <Button
-                className={
-                  gender === 'male'
-                    ? 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg bg-mainColor text-white'
-                    : 'px-[16px] py-[20px] h-auto border min-w-0 rounded-lg border-[#A1A1AA]'
-                }
-                variant={gender === 'male' ? 'solid' : 'bordered'}
-                type="button"
-                onClick={() => onGenderSelect('male')}
-              >
-                남성
-              </Button>
-            </div>
-          </div>
-          {!isValidateShow.nickname && (
-            <p className="text-red-500 text-[13px] mt-2">닉네임은 2-12글자 이내로 작성해주세요. </p>
-          )}
+          ))}
+          <Button
+            className="duration-200 bg-mainColor text-white p-5 mt-[16px] rounded-lg font-semibold w-full py-[20px] h-auto text-[16px]"
+            type="submit"
+          >
+            회원가입
+          </Button>
+        </form>
+        <div className="flex items-center gap-2 justify-center mt-[8px]">
+          <p>이미 계정이 있다면?</p>
+          <Link href="/login" className="text-[#27272A] rounded-lg h-auto text-[16px] underline" type="button">
+            로그인하기
+          </Link>
         </div>
-        {JOIN_FORM_LIST.map(({ type, name, placeholder, error }) => (
-          <label key={name} className="relative">
-            <input
-              className="p-5 border border-[#A1A1AA] placeholder:text-[#A1A1AA] placeholder:text-[14px] rounded-lg focus:outline-none focus:border-mainColor w-full"
-              type={type}
-              name={name}
-              placeholder={placeholder}
-              onChange={onChangeInput}
-              required
-            />
-            {name === 'password' && (
-              <button type="button" onClick={togglePasswordShow} className="absolute top-6 right-4">
-                {passwordShow ? <IoMdEye className="text-xl" /> : <IoMdEyeOff className="text-xl" />}
-              </button>
-            )}
-            {!isValidateShow[name] && <p className="text-red-500 text-[13px] mt-2">{error}</p>}
-          </label>
-        ))}
-        <Button
-          className="duration-200 bg-mainColor text-white p-5 mt-[16px] rounded-lg font-semibold w-full py-[20px] h-auto text-[16px]"
-          type="submit"
-        >
-          회원가입
-        </Button>
-      </form>
-      <div className="flex items-center gap-2 justify-center mt-[8px]">
-        <p>이미 계정이 있다면?</p>
-        <Link href="/login" className="text-[#27272A] rounded-lg h-auto text-[16px] underline" type="button">
-          로그인하기
-        </Link>
       </div>
     </>
   );
