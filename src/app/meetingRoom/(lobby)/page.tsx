e937074054432ae9d0c49b3ad20c28a29c2fb082
injeong Hwang <twilight9758@gmail.com>
2024-05-01 04:50:57 +0900

    [fix/#218] -LobbyPage.tsx 정리

--- src/app/meetingRoom/(lobby)/page.tsx
+++ src/app/meetingRoom/(lobby)/page.tsx
@@ -1,13 +1,9 @@
 import LobbySkeleton from '@/components/room/LobbySkeleton';
 import MeetingRoomList from '@/components/room/lobby/MeetingRoomList';
-import { useGetUserDataQuery } from '@/hooks/useQueries/useUserQuery';
-import { USER_DATA_QUERY_KEY } from '@/query/user/userQueryKeys';
 import { serverSupabase } from '@/utils/supabase/server';
-import { QueryClient, queryOptions } from '@tanstack/react-query';
 import { Suspense } from 'react';
 
 const LobbyPage = async () => {
-  const queryclient = new QueryClient();
   const supabase = serverSupabase();
   const {
     data: { user }
