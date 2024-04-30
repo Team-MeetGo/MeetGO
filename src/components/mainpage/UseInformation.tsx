47062b86260e0b6a795d01dcff4d710b541973be
injeong Hwang <twilight9758@gmail.com>
2024-04-30 16:39:09 +0900

    [style/#218] - 메인페이지 이용방법 스타일 수정

--- src/components/mainpage/UseInformation.tsx
+++ src/components/mainpage/UseInformation.tsx
@@ -4,16 +4,16 @@ import { stepArr } from '@/utils/data/mainPageData';
 
 const UseInformation = () => {
   return (
-    <section className="grid max-sm:grid-cols-1 max-lg:grid-cols-2 grid-cols-4 gap-4 mx-auto py-[16px] px-[36px] w-full max-w-[1080px] bg-gray4 rounded-lg">
+    <section className="grid max-sm:grid-cols-1 max-lg:grid-cols-2 grid-cols-4 gap-2 mx-auto py-[16px] px-[80px] w-full max-w-[1080px] bg-gray4 rounded-lg">
       {stepArr.map((step) => (
-        <div key={step.stepNum} className="flex flex-col gap-10">
-          <div className="flex flex-col gap-1.5">
+        <div key={step.stepNum} className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1.5 px-[8px]">
             <div className="flex items-center justify-center w-14 h-5 bg-secondMainColor rounded-full text-2xs text-white">
               STEP {step.stepNum}
             </div>
             <h2 className="text-xl font-bold">{step.title}</h2>
           </div>
-          <div className="relative h-40">
+          <div className="relative w-52 h-40 flex justify-center">
             <Image
               src={step.src}
               alt="이용방법"
