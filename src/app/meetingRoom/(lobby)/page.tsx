8c06673e62068ab271fd8d687ba16a1b1d816138
injeong Hwang <twilight9758@gmail.com>
2024-05-04 07:31:35 +0900

    [fix/#218] - LobbyPage.tsx 불필요한 getUser() 요청 삭제

--- src/app/meetingRoom/(lobby)/page.tsx
+++ src/app/meetingRoom/(lobby)/page.tsx
@@ -1,15 +1,9 @@
 import LobbySkeleton from '@/components/room/LobbySkeleton';
 import LobbyBanner from '@/components/room/lobby/LobbyBanner';
 import MeetingRoomList from '@/components/room/lobby/MeetingRoomList';
-import ThinBanner from '@/utils/banner/ThinBanner';
-import { serverSupabase } from '@/utils/supabase/server';
 import { Suspense } from 'react';
 
 const LobbyPage = async () => {
-  const supabase = serverSupabase();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
   return (
     <>
       <Suspense fallback={<LobbySkeleton />}>
