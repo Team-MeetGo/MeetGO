43aa15fbbbfde360246764fe65f463c2be80b9be
injeong Hwang <twilight9758@gmail.com>
2024-05-04 07:17:13 +0900

    [fix/#218] - 에러바운더리 실험중

--- src/app/chat/[chatroom_id]/page.tsx
+++ src/app/chat/[chatroom_id]/page.tsx
@@ -40,6 +40,10 @@ const ChatPage = async ({ params }: { params: { chatroom_id: string } }) => {
     queryFn: prefetchMsgs
   });
 
+  const abc = 0;
+
+  if (abc === 0) throw new Error('에러!!');
+
   return (
     <main>
       <HydrationBoundary state={dehydrate(queryClient)}>
