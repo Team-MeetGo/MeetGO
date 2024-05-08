9852f625d437d65a0111e4808e45d64421bde3d6
injeong Hwang <twilight9758@gmail.com>
2024-05-09 04:13:55 +0900

    [refactor/#303] - 수락창 리팩토링

--- src/components/room/participants/MaleOrFemale.tsx
+++ src/components/room/participants/MaleOrFemale.tsx
@@ -3,16 +3,18 @@
 import { UsersType } from '@/types/userTypes';
 import { FaCrown } from 'react-icons/fa6';
 import Image from 'next/image';
-import { RoomFemaleAvatar } from '@/utils/icons/RoomAvatar';
+import { RoomFemaleAvatar, RoomMaleAvatar } from '@/utils/icons/RoomAvatar';
 import { IoFemale } from 'react-icons/io5';
-import MeetGoLogoPurple from '@/utils/icons/meetgo-logo-purple.png';
+import HollowFemaleMemberCard from './HollowMemberCard';
+import { GENDERFILTER } from '@/utils/constant';
 
 const MaleOrFemale = ({
+  gender,
   genderList,
   leader
 }: {
+  gender: string;
   genderList: (UsersType | null)[];
-  //   arr: (UsersType | null)[];
   leader: string;
 }) => {
   return (
@@ -21,21 +23,23 @@ const MaleOrFemale = ({
     >
       {genderList.map((member, idx) =>
         member ? (
-          <div key={member?.user_id} className={`grid col-start-1 col-span-1`}>
+          <div key={member.user_id} className={`grid col-start-1 col-span-1`}>
             <article
-              className={`border-purpleThird border-[2px] flex flex-row align-middle lg:w-[490px] w-[20rem] lg:h-[166px] h-[8rem] rounded-2xl`}
+              className={`${
+                gender === GENDERFILTER.FEMALE ? 'border-[2px] border-purpleThird' : 'bg-purpleSecondary'
+              }  flex flex-row align-middle lg:w-[490px] w-[20rem] lg:h-[166px] h-[8rem] rounded-2xl`}
             >
               {/* 카드 왼쪽 */}
               <div className="lg:mx-[40px] mx-[1rem] lg:h-[86px] w-[5rem] flex flex-col justify-items-center">
                 <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] lg:mt-[32px] mt-[0.8rem] rounded-full relative">
-                  {leader === member?.user_id ? (
+                  {leader === member.user_id ? (
                     <div>
                       <FaCrown className="absolute h-[20px] w-[20px] m-[2px] fill-mainColor z-5 top-[-18px] left-[29px]" />
                     </div>
                   ) : (
                     ''
                   )}
-                  {member?.avatar ? (
+                  {member.avatar ? (
                     <Image
                       className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill"
                       src={member.avatar}
@@ -46,11 +50,11 @@ const MaleOrFemale = ({
                     />
                   ) : (
                     <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill">
-                      <RoomFemaleAvatar />
+                      {gender === GENDERFILTER.FEMALE ? <RoomFemaleAvatar /> : <RoomMaleAvatar />}
                     </figure>
                   )}
                   <p className="lg:pt-[8px] pt-[0.4rem] w-full lg:text-[16px] text-[0.9rem] text-center">
-                    {member?.nickname}
+                    {member.nickname}
                   </p>
                 </figure>
               </div>
@@ -63,7 +67,7 @@ const MaleOrFemale = ({
                 </div>
                 <figure className="flex flex-row w-100% text-[14px] w-100%">
                   <div className="lg:my-[16px] my-[0.5rem] flex flex-row gap-[6px]">
-                    {member?.favorite?.map((tag) => (
+                    {member.favorite?.map((tag) => (
                       <div
                         key={tag}
                         className="bg-purpleSecondary text-mainColor lg:text-[14px] text-[0.7rem] rounded-[8px] lg:p-[8px] p-[0.3rem]"
@@ -73,23 +77,14 @@ const MaleOrFemale = ({
                     ))}
                   </div>
                 </figure>
-                <h2 className="text-[14px]">{member?.intro}</h2>
+                <h2 className="text-[14px]">{member.intro}</h2>
               </div>
             </article>
           </div>
         ) : (
-          <Image
-            key={idx}
-            className="w-[86px] h-[68px] object-center flex justify-center my-auto"
-            src={MeetGoLogoPurple}
-            alt="참여하지 않은 인원"
-            height={80}
-            width={80}
-            sizes="86px"
-          />
+          <HollowFemaleMemberCard key={idx} gender={gender} />
         )
       )}
-      {/* <HollowFemaleMemberCard array={hollowFemaleArray} /> */}
     </section>
   );
 };
--- src/components/room/participants/Member.tsx
+++ src/components/room/participants/Member.tsx
@@ -3,30 +3,18 @@
 import GotoChatButton from '@/components/room/participants/GotoChatButton';
 import { useRoomInformationQuery } from '@/hooks/useQueries/useMeetingQuery';
 import { GENDERFILTER } from '@/utils/constant';
-import { RoomFemaleAvatar, RoomMaleAvatar } from '@/utils/icons/RoomAvatar';
 import { clientSupabase } from '@/utils/supabase/client';
-import { genderMemberNumber } from '@/utils/utilFns';
-import Image from 'next/image';
+import { genderMemberNumber, handleGenderList } from '@/utils/utilFns';
 import { useEffect, useState } from 'react';
-import { FaCrown } from 'react-icons/fa6';
-import { IoFemale, IoMale } from 'react-icons/io5';
-import HollowFemaleMemberCard from './HollowFemaleMemberCard';
-import HollowMaleMemberCard from './HollowMaleMemberCard';
-
 import type { UserType } from '@/types/roomTypes';
 import type { UUID } from 'crypto';
+import MaleOrFemale from './MaleOrFemale';
 
 const Member = ({ roomId }: { roomId: UUID }) => {
   const { roomMemberWithId: participants, roomInfoWithId: roomInformation } = useRoomInformationQuery(roomId);
-
   const [members, setMembers] = useState<UserType[]>(participants);
   const [leader, setLeader] = useState(roomInformation.leader_id as string);
-
-  const genderMaxNumber = genderMemberNumber(roomInformation?.member_number as string);
-  const femaleMembers = members.filter((member) => member.gender === GENDERFILTER.FEMALE);
-  const maleMembers = members.filter((member) => member.gender === GENDERFILTER.MALE);
-  const hollowFemaleArray = Array.from({ length: (genderMaxNumber as number) - femaleMembers.length }, (_, i) => i);
-  const hollowMaleArray = Array.from({ length: (genderMaxNumber as number) - maleMembers.length }, (_, i) => i);
+  const genderMaxNumber = genderMemberNumber(roomInformation.member_number as string);
 
   useEffect(() => {
     setLeader(roomInformation?.leader_id!);
@@ -97,133 +85,16 @@ const Member = ({ roomId }: { roomId: UUID }) => {
         </section>
 
         <section className="flex lg:flex-row flex-col lg:gap-[100px] gap-[2rem] lg:min-w-[1080px] w-[22rem] items-center justify-content align-middle ">
-          <section
-            className={`lg:mt-[40px] mt-[2rem] grid grid-cols-1 grid-rows-${femaleMembers.length} w-full lg:gap-y-[50px] gap-y-[0.8rem]`}
-          >
-            {femaleMembers.map((member) => (
-              <div key={member.user_id} className={`grid col-start-1 col-span-1`}>
-                <article
-                  className={`border-purpleThird border-[2px] flex flex-row align-middle lg:w-[490px] w-[20rem] lg:h-[166px] h-[8rem] rounded-2xl`}
-                >
-                  {/* 카드 왼쪽 */}
-                  <div className="lg:mx-[40px] mx-[1rem] lg:h-[86px] w-[5rem] flex flex-col justify-items-center">
-                    <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] lg:mt-[32px] mt-[0.8rem] rounded-full relative">
-                      {leader === member.user_id ? (
-                        <div>
-                          <FaCrown className="absolute h-[20px] w-[20px] m-[2px] fill-mainColor z-5 top-[-18px] left-[29px]" />
-                        </div>
-                      ) : (
-                        ''
-                      )}
-                      {member.avatar ? (
-                        <Image
-                          className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill"
-                          src={member.avatar}
-                          alt="유저 아바타"
-                          height={86}
-                          width={86}
-                          sizes="86px"
-                        />
-                      ) : (
-                        <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill">
-                          <RoomFemaleAvatar />
-                        </figure>
-                      )}
-                      <p className="lg:pt-[8px] pt-[0.4rem] w-full lg:text-[16px] text-[0.9rem] text-center">
-                        {member.nickname}
-                      </p>
-                    </figure>
-                  </div>
-
-                  {/* 카드 오른쪽 */}
-                  <div className="flex flex-col justify-center w-full">
-                    <div className="flex flex-row text-[16px]">
-                      <h2 className="pr-[4px]">{member.school_name}</h2>
-                      {<IoFemale className="w-[14px] my-auto fill-hotPink" />}
-                    </div>
-                    <figure className="flex flex-row w-full text-[14px]">
-                      <div className="lg:my-[16px] my-[0.5rem] flex flex-row gap-[6px]">
-                        {member.favorite?.map((tag) => (
-                          <div
-                            key={tag}
-                            className="bg-purpleSecondary text-mainColor lg:text-[14px] text-[0.7rem] rounded-[8px] lg:p-[8px] p-[0.3rem]"
-                          >
-                            {tag}
-                          </div>
-                        ))}
-                      </div>
-                    </figure>
-                    <h2 className="text-[14px]">{member.intro}</h2>
-                  </div>
-                </article>
-              </div>
-            ))}
-            <HollowFemaleMemberCard array={hollowFemaleArray} />
-          </section>
-
-          <section
-            className={`lg:mt-[40px] mt-[2rem] grid grid-cols-1 grid-rows-${maleMembers.length} w-full lg:gap-y-[50px] gap-y-[0.8rem] lg:mb-0 mb-[4rem]`}
-          >
-            {maleMembers.map((member) => (
-              <div key={member.user_id} className={`grid col-start-1 col-span-1`}>
-                <article
-                  className={`bg-purpleSecondary flex flex-row align-middle lg:w-[490px] w-[20rem] lg:h-[166px] h-[8rem] rounded-2xl`}
-                >
-                  {/* 카드 왼쪽 */}
-                  <div className="lg:mx-[40px] mx-[1rem] w-[5rem] flex flex-col items-center justify-content align-middle">
-                    <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] lg:mt-[32px] mt-[0.8rem] rounded-full relative">
-                      {leader === member.user_id ? (
-                        <div>
-                          <FaCrown className="absolute h-[20px] w-[20px] m-[2px] fill-mainColor z-5 top-[-20px] left-[30px]" />
-                        </div>
-                      ) : (
-                        ''
-                      )}
-                      {member.avatar ? (
-                        <Image
-                          className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill"
-                          src={member.avatar}
-                          alt="유저 아바타"
-                          height={80}
-                          width={80}
-                          sizes="86px"
-                        />
-                      ) : (
-                        <figure className="lg:w-[86px] w-[5rem] lg:h-[86px] h-[5rem] rounded-full object-center bg-cover object-fill">
-                          <RoomMaleAvatar />
-                        </figure>
-                      )}
-                      <p className="lg:pt-[8px] pt-[0.4rem] w-full lg:text-[16px] text-[0.9rem] text-center">
-                        {member.nickname}
-                      </p>
-                    </figure>
-                  </div>
-
-                  {/* 카드 오른쪽 */}
-                  <div className="flex flex-col justify-center">
-                    <div className="flex flex-row gap text-[16px]">
-                      <h2 className="pr-[4px]">{member.school_name}</h2>
-                      {<IoMale className="w-[14px] my-auto fill-blue" />}
-                    </div>
-                    <figure className="flex flex-row w-full">
-                      <div className="lg:my-[16px] my-[0.5rem] flex flex-row gap-[6px]">
-                        {member.favorite?.map((tag) => (
-                          <div
-                            key={tag}
-                            className="bg-white text-mainColor lg:text-[14px] text-[0.7rem] rounded-[8px] lg:p-[8px] p-[0.3rem]"
-                          >
-                            {tag}
-                          </div>
-                        ))}
-                      </div>
-                    </figure>
-                    <h2 className="text-[14px]">{member.intro}</h2>
-                  </div>
-                </article>
-              </div>
-            ))}
-            <HollowMaleMemberCard array={hollowMaleArray} />
-          </section>
+          <MaleOrFemale
+            gender={GENDERFILTER.FEMALE}
+            genderList={handleGenderList(genderMaxNumber, participants, GENDERFILTER.FEMALE)}
+            leader={leader}
+          />
+          <MaleOrFemale
+            gender={GENDERFILTER.MALE}
+            genderList={handleGenderList(genderMaxNumber, participants, GENDERFILTER.MALE)}
+            leader={leader}
+          />
         </section>
       </div>
     </div>
