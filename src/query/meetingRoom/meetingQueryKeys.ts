8764c1b3232963b111ea90429d1734e6c31138c9
injeong Hwang <twilight9758@gmail.com>
2024-05-09 14:54:01 +0900

    [fix/#303] - 미팅룸 쿼리키 일관성 있게 수정

--- src/query/meetingRoom/meetingQueryKeys.ts
+++ src/query/meetingRoom/meetingQueryKeys.ts
@@ -12,6 +12,6 @@ export const ROOMLIST = 'roomList';
 
 export const CHATROOM_ID = 'chatRoom_id_with_room_id';
 
-export const MY_PAST_NOW_ROOM = ['my_past_now_room'];
+export const MY_PAST_NOW_ROOM = 'my_past_now_room';
 
-export const ROOMDATA = ['ROOMDATA'];
+export const ROOMDATA = 'roomData';
