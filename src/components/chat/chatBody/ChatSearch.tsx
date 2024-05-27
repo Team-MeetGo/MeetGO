69b608f5e782edd38523123f164286fefb50a682
injeong Hwang <twilight9758@gmail.com>
2024-05-27 20:16:43 +0900

    [fix/#303] - Suspense 오류 해결 후 ChatList 렌더링 전 skeleton 추가

--- src/components/chat/chatBody/ChatSearch.tsx
+++ src/components/chat/chatBody/ChatSearch.tsx
@@ -37,6 +37,8 @@ const ChatSearch = ({ isScrollTop }: { isScrollTop: boolean }) => {
           setUpDownCount((prev) => prev + 1);
           // upDownCount는 지금까지 search한 것을 기반으로 되어야 하니까 여기에 종속되어서 +
         }
+      } else if (!idsDivs) {
+        alert('찾으시는 내용이 없습니다.');
       } else {
         alert('더 이상 찾을 내용이 없습니다.');
       }
