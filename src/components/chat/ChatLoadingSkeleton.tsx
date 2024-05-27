54f38f6fec0555ff7de92b243ec1eac4863d937a
injeong Hwang <twilight9758@gmail.com>
2024-05-27 21:50:50 +0900

    [fix/#303] - skeleton css 수정

--- src/components/chat/ChatLoadingSkeleton.tsx
+++ src/components/chat/ChatLoadingSkeleton.tsx
@@ -2,9 +2,13 @@ import { Skeleton } from '@nextui-org/skeleton';
 
 const ChatLoadingSkeleton = () => {
   return (
-    <div className="w-full flex flex-col gap-2 mx-auto my-auto">
+    <div className="w-full h-full flex flex-col gap-8 px-16 justify-center">
+      <Skeleton className="h-3 w-2/5 rounded-lg" />
+      <Skeleton className="ml-auto h-3 w-2/5 rounded-lg" />
       <Skeleton className="h-3 w-3/5 rounded-lg" />
-      <Skeleton className="h-3 w-4/5 rounded-lg" />
+      <Skeleton className="ml-auto h-3 w-3/5 rounded-lg" />
+      <Skeleton className="h-3 w-1/5 rounded-lg" />
+      <Skeleton className="ml-auto h-3 w-2/5 rounded-lg" />
     </div>
   );
 };
